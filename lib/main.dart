e7c3298f388553e5c03bd71646aa098aa3ebc362
maxxkim <maxxxkim@ya.ru>
2023-01-09 19:29:52 +0300

    Hi mark

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -58,8 +60,14 @@ class _MyHomePageState extends State<MyHomePage> {
       case 0:
         page = GeneratorPage();
         break;
+      case 2:
+        page = ContactsPage();
+        break;
       case 1:
-        page = FavoritesPage();
+        page = AchievementsPage();
+        break;
+      case 3:
+        page = MotivationPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -77,8 +85,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     label: Text('Home'),
                   ),
                   NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
+                    icon: Icon(Icons.done),
+                    label: Text('My Achievements'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.chat),
+                    label: Text('Chats'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.sunny),
+                    label: Text('Motivation'),
                   ),
                 ],
                 selectedIndex: selectedIndex, // ← Change to this.
@@ -111,16 +127,55 @@ class GeneratorPage extends StatelessWidget {
 
     IconData icon;
     if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
+      icon = Icons.share;
     } else {
-      icon = Icons.favorite_border;
+      icon = Icons.share;
     }
 
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          BigCard(pair: pair),
+          Text("You havent't been smoking for"),
+          SizedBox(height: 10),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "2 months 14 days 3 hours and 14 minutes",
+                semanticsLabel: pair.asPascalCase,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('This also means'),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    "76 packs",
+                    semanticsLabel: pair.asPascalCase,
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              Text('or'),
+              SizedBox(width: 10),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    "248 \$",
+                    semanticsLabel: pair.asPascalCase,
+                  ),
+                ),
+              ),
+            ],
+          ),
           SizedBox(height: 10),
           Row(
             mainAxisSize: MainAxisSize.min,
@@ -130,16 +185,79 @@ class GeneratorPage extends StatelessWidget {
                   appState.toggleFavorite();
                 },
                 icon: Icon(icon),
-                label: Text('Like'),
+                label: Text('Share'),
               ),
               SizedBox(width: 10),
               ElevatedButton(
                 onPressed: () {
                   appState.getNext();
                 },
-                child: Text('Next'),
+                child: Text('Edit'),
               ),
             ],
+          ),
+          SizedBox(height: 20),
+          Text('Your next goal is not to smoke for'),
+          SizedBox(height: 10),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "14 days 20 hours and 46 minutes",
+                semanticsLabel: pair.asPascalCase,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text('Good luck!'),
+        ],
+      ),
+    );
+  }
+}
+
+class AchievementsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ListView(
+        children: [
+          Text('Your stats for today'),
+          SizedBox(height: 10),
+          Text('Cigarettes smoked:'),
+          SizedBox(height: 10),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "You did not smoke!",
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('Cravings felt:'),
+          SizedBox(height: 10),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "2",
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('Goals available:'),
+          SizedBox(height: 10),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "Your lungs are way cleaner now!",
+              ),
+            ),
           ),
         ],
       ),
@@ -147,26 +265,86 @@ class GeneratorPage extends StatelessWidget {
   }
 }
 
-class FavoritesPage extends StatelessWidget {
+class ContactsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var words = appState.favorites;
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          words.forEach(
-            (word) {
-                BigCard(pair: word),
-                SizedBox(height: 10),
-            }
-          );
-                  BigCard(pair: words[0]),
-          SizedBox(height: 10),
-        ],
-      ),
+    return ListView(
+      children: [
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(size: 56.0),
+            title: Text('Joseph Mengele'),
+            subtitle: Text('Your psychiatrist'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(size: 56.0),
+            title: Text('Edward Alrick'),
+            subtitle: Text('Friend'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(size: 56.0),
+            title: Text('Alina Fox'),
+            subtitle: Text('Friend'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MotivationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('Motivation'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorite saying:'),
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              "'It's easy to quit smoking. I've done it hundreds of times.' (c) Mark Twain",
+            ),
+          ),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                appState.toggleFavorite();
+              },
+              child: Text('Share'),
+            ),
+            SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Edit'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
@@ -182,7 +360,7 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    var style = theme.textTheme.displayMedium!.copyWith(
+    var style = theme.textTheme.displaySmall!.copyWith(
       color: theme.colorScheme.onPrimary,
     );
     return Card(
@@ -190,7 +368,7 @@ class BigCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Text(
-          pair.asLowerCase,
+          "2 months 14 days 3 hours and 14 minutes",
           style: style,
           semanticsLabel: pair.asPascalCase,
         ),
